c53ff0f58ad40e6f22d2898c914318e32dbdd83d
Venny-Shiru <vennywanjiru@gmail.com>
2025-09-18 15:30:34 +0300

    Fix CORS issue by removing conflicting wildcard override

--- server/index.js
+++ server/index.js
@@ -106,17 +106,6 @@ app.use('/uploads', (req, res, next) => {
   }
 }, express.static('server/uploads'));
 
-// TEMP: Allow all origins for CORS (for debugging only)
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
 
 // Routes
 app.use('/api/auth', authRoutes);
